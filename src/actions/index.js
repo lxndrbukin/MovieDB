1467d4d964486d63f7629736fd5319fdef86af3c
lxndrbukin <alex.bukin@hotmail.com>
2023-03-10 22:33:24 +0000

    Fixing API URL issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,17 @@
 import Box from '../components/Box';
 
+const api = 'https://www.omdbapi.com';
 const apikey = 'a4daa08f';
 
 export const fetchMovies = async (searchTerm) => {
   const movieList = [];
-  const res = await fetch(
-    `https//www.omdbapi.com/?apikey=${apikey}&s=${searchTerm}`
-  );
+  const res = await fetch(`${api}/?apikey=${apikey}&s=${searchTerm}`);
 
   if (searchTerm) {
     const moviesData = await res.json();
     const movieIDs = moviesData.Search.map((movie) => movie.imdbID);
     for (let movieID of movieIDs) {
-      const res = await fetch(
-        `https://www.omdbapi.com/?apikey=${apikey}&i=${movieID}`
-      );
+      const res = await fetch(`${api}/?apikey=${apikey}&i=${movieID}`);
       const data = await res.json();
       try {
         movieList.push(data);
